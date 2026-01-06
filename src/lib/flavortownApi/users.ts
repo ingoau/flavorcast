37be057b19ef2ca0c08e79aaf6471c16094cf1b0
Ingo Wolf <me@ingowolf.au>
2026-01-06 21:24:32 +1100

    allow string as user type

--- src/lib/flavortownApi/users.ts
+++ src/lib/flavortownApi/users.ts
@@ -2,7 +2,7 @@ import { API_BASE } from "../constants";
 import client from "./client";
 import { User } from "./types";
 
-export async function get(userId: number) {
+export async function get(userId: string) {
   const response = await client.get<User>(`${API_BASE}/users/${userId}`);
   return response.data as User;
 }
