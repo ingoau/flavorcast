a7607aee903f4793d3051da43c0e617524d1ab6d
Ingo Wolf <me@ingowolf.au>
2026-01-06 21:30:43 +1100

    use current signed in user

--- src/lib/listUserProjects.ts
+++ src/lib/listUserProjects.ts
@@ -1,7 +1,7 @@
 import { flavortownApi } from "./flavortownApi";
 
-export default async function listUserProjects(userId: string) {
-  const user = await flavortownApi.users.get(userId);
+export default async function listUserProjects() {
+  const user = await flavortownApi.users.get("me");
   return await Promise.all(
     user.project_ids.map(async (projectId) => {
       const project = await flavortownApi.projects.get(projectId);
