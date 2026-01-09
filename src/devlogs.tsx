b9dfae8f85e9be4a8c26b64a480e619987340f46
Ingo Wolf <me@ingowolf.au>
2026-01-09 17:00:12 +1100

    add devlogs page

---
+++ src/devlogs.tsx
@@ -0,0 +1,17 @@
+import { List } from "@raycast/api";
+import { usePromise } from "@raycast/utils";
+import { flavortownApi } from "./lib/flavortownApi";
+
+export default function Devlogs(projectId: number) {
+  const { isLoading, data } = usePromise(() => {
+    return flavortownApi.devlogs.list(projectId);
+  });
+
+  return (
+    <List isLoading={isLoading} isShowingDetail>
+      {data?.devlogs.map((devlog) => (
+        <List.Item key={devlog.id} title={devlog.body} subtitle={devlog.created_at} />
+      ))}
+    </List>
+  );
+}
