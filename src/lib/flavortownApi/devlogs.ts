8b2734f0eb21f9b72add99e0dc7baa6ce5929b1b
Ingo Wolf <me@ingowolf.au>
2026-01-09 16:58:47 +1100

    add devlogs listing thing

--- src/lib/flavortownApi/devlogs.ts
+++ src/lib/flavortownApi/devlogs.ts
@@ -5,3 +5,12 @@ export async function get(projectId: number, devlogId: number) {
   const response = await client.get<Devlog>(`projects/${projectId}/devlogs/${devlogId}`);
   return response.data as Devlog;
 }
+
+export async function list(projectId: number) {
+  const response = await client.get<{
+    devlogs: Devlog[];
+  }>(`projects/${projectId}/devlogs`);
+  return response.data as {
+    devlogs: Devlog[];
+  };
+}
